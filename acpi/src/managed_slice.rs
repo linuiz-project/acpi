c98f00a47b21fdb30ba544ae80d36d51b7bd11ed
Zavier Divelbiss <semiviral@gmail.com>
2022-11-15 22:38:17 -0600

    fix UB in `ManagedSlice` API

--- acpi/src/managed_slice.rs
+++ acpi/src/managed_slice.rs
@@ -1,65 +1,83 @@
-use core::{alloc, mem};
+use core::{
+    alloc::{self, AllocError, Layout},
+    mem::MaybeUninit,
+    ptr::NonNull,
+};
 
 /// Thin wrapper around a regular slice, taking a reference to an allocator for automatic
 /// deallocation when the slice is dropped out of scope.
 #[derive(Debug)]
-pub struct ManagedSlice<'a, T, A>
+pub struct ManagedSlice<T, A>
 where
     A: alloc::Allocator,
 {
-    slice: &'a mut [T],
-    allocator: &'a A,
+    memory: NonNull<[T]>,
+    allocator: A,
 }
 
-impl<'a, T, A> ManagedSlice<'a, T, A>
+impl<T, A> ManagedSlice<T, A>
+where
+    A: alloc::Allocator,
+{
+    pub fn new_uninit_in(len: usize, allocator: A) -> Result<ManagedSlice<MaybeUninit<T>, A>, AllocError> {
+        let layout = Layout::array::<T>(len).map_err(|_| AllocError)?;
+        allocator.allocate(layout).map(|ptr| ManagedSlice::<MaybeUninit<T>, A> {
+            memory: NonNull::slice_from_raw_parts(ptr.as_non_null_ptr().cast(), len),
+            allocator,
+        })
+    }
+}
+
+impl<T: Copy, A> ManagedSlice<T, A>
 where
     A: alloc::Allocator,
 {
     /// Attempts to allocate a new `&mut [T]` in the given allocator.
-    pub fn new_in(len: usize, allocator: &'a A) -> crate::AcpiResult<Self> {
-        // Safety: Struct layouts are required to be valid.
-        let layout =
-            unsafe { alloc::Layout::from_size_align_unchecked(mem::size_of::<T>() * len, mem::align_of::<T>()) };
+    pub fn new_in(len: usize, value: T, allocator: A) -> Result<Self, AllocError> {
+        let layout = Layout::array::<T>(len).map_err(|_| AllocError)?;
 
-        unsafe { allocator.allocate(layout).map(|ptr| ptr.as_uninit_slice_mut().align_to_mut::<T>().1) }
-            .map(|slice| Self { slice, allocator })
-            .map_err(|_| crate::AcpiError::AllocError)
+        allocator
+            .allocate(layout)
+            .map(|ptr| NonNull::slice_from_raw_parts(ptr.as_non_null_ptr().cast::<T>(), len))
+            .map(|memory| {
+                // We must properly initialize the contents of the slice to avoid returning `ManagedSlice<MaybeUninit<T>>`.
+                unsafe { memory.as_uninit_slice_mut().fill(core::mem::MaybeUninit::new(value)) };
+                Self { memory, allocator }
+            })
     }
 }
 
-impl<'a, T, A> Drop for ManagedSlice<'a, T, A>
+impl<T, A> Drop for ManagedSlice<T, A>
 where
     A: alloc::Allocator,
 {
     fn drop(&mut self) {
-        // Safety: Slice is required by function to point to non-null memory.
-        let slice_ptr = unsafe { core::ptr::NonNull::new_unchecked(self.slice.as_ptr().cast_mut().cast::<u8>()) };
-        // Safety: Slice is constructed from a valid layout.
-        let slice_layout = unsafe {
-            alloc::Layout::from_size_align_unchecked(mem::size_of_val(self.slice), mem::align_of_val(self.slice))
-        };
+        let ptr = self.memory.as_non_null_ptr().cast();
+        let layout = Layout::array::<T>(self.len()).unwrap();
 
         // Safety: Caller is required to provide a slice allocated with the provided allocator.
-        unsafe { self.allocator.deallocate(slice_ptr, slice_layout) };
+        unsafe { self.allocator.deallocate(ptr, layout) };
     }
 }
 
-impl<'a, T, A> core::ops::Deref for ManagedSlice<'a, T, A>
+impl<T, A> core::ops::Deref for ManagedSlice<T, A>
 where
     A: alloc::Allocator,
 {
     type Target = [T];
 
     fn deref(&self) -> &Self::Target {
-        self.slice
+        // Safety: Type always initializes memory to a valid T.
+        unsafe { self.memory.as_ref() }
     }
 }
 
-impl<'a, T, A> core::ops::DerefMut for ManagedSlice<'a, T, A>
+impl<T, A> core::ops::DerefMut for ManagedSlice<T, A>
 where
     A: alloc::Allocator,
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        self.slice
+        // Safety: Type always initializes memory to a valid T.
+        unsafe { self.memory.as_mut() }
     }
 }
